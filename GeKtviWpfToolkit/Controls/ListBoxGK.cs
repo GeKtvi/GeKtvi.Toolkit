f9ed35f9a4e4106d4d26c6e75ec09e6ac70f5272
GeKtvi <61162497+GeKtvi@users.noreply.github.com>
2023-05-09 20:01:26 +0300

    hz

---
+++ GeKtviWpfToolkit/Controls/ListBoxGK.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows;
+using System.Collections;
+
+namespace GeKtviWpfToolkit.Controls
+{
+    public class ListBoxGK : ListBox
+    {
+        internal bool processSelectionChanges = false;
+
+        public static readonly DependencyProperty BindableSelectedItemsProperty =
+            DependencyProperty.Register(nameof(BindableSelectedItems),
+                typeof(object), typeof(ListBoxGK),
+                new FrameworkPropertyMetadata(null,
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(OnBindableSelectedItemsChanged)));
+
+        public IList BindableSelectedItems
+        {
+            get => (IList)GetValue(BindableSelectedItemsProperty);
+            set => SetValue(BindableSelectedItemsProperty, value);
+        }
+
+        protected override void OnSelectionChanged(SelectionChangedEventArgs e)
+        {
+            base.OnSelectionChanged(e);
+
+            if (BindableSelectedItems == null || !IsInitialized) return; //Handle pre initilized calls
+
+            IList selectedItems = BindableSelectedItems;
+
+            if (e.AddedItems.Count > 0)
+                if (!string.IsNullOrWhiteSpace(SelectedValuePath))
+                {
+                    foreach (var item in e.AddedItems)
+                        if (!selectedItems.Contains(item.GetType().GetProperty(SelectedValuePath).GetValue(item, null)))
+                            selectedItems.Add(item.GetType().GetProperty(SelectedValuePath).GetValue(item, null));
+                }
+                else
+                {
+                    foreach (var item in e.AddedItems)
+                        if (!selectedItems.Contains(item))
+                            selectedItems.Add(item);
+                }
+
+            if (e.RemovedItems.Count > 0)
+                if (!string.IsNullOrWhiteSpace(SelectedValuePath))
+                {
+                    foreach (var item in e.RemovedItems)
+                        if (selectedItems.Contains(item.GetType().GetProperty(SelectedValuePath).GetValue(item, null)))
+                            selectedItems.Remove(item.GetType().GetProperty(SelectedValuePath).GetValue(item, null));
+                }
+                else
+                {
+                    foreach (var item in e.RemovedItems)
+                        if (selectedItems.Contains(item))
+                            selectedItems.Remove(item);
+                }
+        }
+
+        private static void OnBindableSelectedItemsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ListBoxGK listBox)
+            {
+                List<dynamic> newSelection = new List<dynamic>();
+                if (!string.IsNullOrWhiteSpace(listBox.SelectedValuePath))
+                    foreach (var item in listBox.BindableSelectedItems)
+                    {
+                        foreach (var lbItem in listBox.Items)
+                        {
+                            var lbItemValue = lbItem.GetType().GetProperty(listBox.SelectedValuePath).GetValue(lbItem, null);
+                            if (lbItemValue == item)
+                                newSelection.Add(lbItem);
+                        }
+                    }
+                else
+                    newSelection = listBox.BindableSelectedItems as List<dynamic>;
+
+                listBox.SetSelectedItems(newSelection);
+            }
+        }
+    }
+}
---
+++ GeKtviWpfToolkit/ValueConverters/BooleanToVisibilityConverterReverse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace GeKtviWpfToolkit.ValueConverters
+{
+    internal class BooleanToVisibilityConverterReverse : IValueConverter
+    {
+        private BooleanToVisibilityConverter _baseConverter = new BooleanToVisibilityConverter();
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return _baseConverter.Convert(!(bool)value, targetType, parameter, culture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool)_baseConverter.ConvertBack(value, targetType, parameter, culture);
+        }
+    }
+}
