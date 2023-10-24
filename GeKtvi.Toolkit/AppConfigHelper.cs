a747c79cccc67c58cf2b8e9bdfcd7cfe15fb0af0
GeKtvi <61162497+GeKtvi@users.noreply.github.com>
2023-10-24 19:48:29 +0300

    AppConfigHelper nullable fix

--- GeKtvi.Toolkit/AppConfigHelper.cs
+++ GeKtvi.Toolkit/AppConfigHelper.cs
@@ -6,7 +6,7 @@ namespace GeKtvi.Toolkit
 {
     public class AppConfigHelper
     {
-        public static T LoadConfig<T>(string fileName = "AppConfig.config", string folder = "Properties\\Configs")
+        public static T LoadConfig<T>(string fileName = "AppConfig.config", string folder = "Configs")
         {
             T config;
             string fullPath = Path.Combine(folder, fileName);
@@ -16,12 +16,12 @@ namespace GeKtvi.Toolkit
             XmlSerializer xml = new(typeof(T));
 
             using FileStream fs = new(fullPath, FileMode.Open, FileAccess.Read);
-            config = (T)xml.Deserialize(fs);
+            config = (T)(xml.Deserialize(fs) ?? throw new InvalidDataException($"Cant cast serialized object to {nameof(T)}"));
 
             return config;
         }
 
-        public static void WriteConfig(object config, string fileName = "AppConfig.config", string folder = "Properties\\Configs")
+        public static void WriteConfig(object config, string fileName = "AppConfig.config", string folder = "Configs")
         {
             string fullPath = Path.Combine(folder, fileName);
 
